c3587d36ee0cc203a2ec0924fa7eed6bad4e177b
Yuvraj Maurya <yuvrajmaurya855420@gmail.com>
2026-04-17 18:25:40 +0530

    prisma generated

--- index.mjs
+++ index.mjs
@@ -1,4 +1,5 @@
 import express from 'express'
+import {prisma} from './prisma/prisma_client.mjs'
 const server = express()
 const port =5000
 
@@ -11,6 +12,20 @@ server.use(express.json())
 server.post("/login", (req , res) => {
     console.log(req.body)
     res.status(200).json({name:"success"})
+})
+
+server.post("/signup", async (req , res) => {
+    console.log(req.body)
+    const users = await Promise.users.create({
+        data:{
+            password: req.body.password,
+            email: req.body.email,
+            name: req.body.name,
+
+        }
+    })
+
+    res.json({name:"signup"})
 })
 
 
